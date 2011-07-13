e3cd5003c13a5920a34892189b847f0e23506d77
Giles Chamberlin <gilecham@cisco.com>
2011-07-13 16:22:51 +0100

    Full API impl, not yet using cc module or web module.  Basic tests in place.

--- cart.erl
+++ cart.erl
@@ -4,21 +4,29 @@
 -module(cart).
 
 %% API
--export([start_link/1, donuts/2, macarons/2, danish/2, cupcakes/2, view_cart/1]).
+-export([start_link/1, donuts/2, macarons/2, danish/2, cupcakes/2, view_cart/1,
+	billing_address/2, credit_card/3, buy/1]).
 
 
 %% Private interfaces
--export([init/2]).
+-export([init/2, test/0]).
 
 
+
+-record(user, {name, address, card}).
 
 %% Cart is initialised with a price list representing all valid order items
 
 %% API Definitions
 
+
+%% Whilst reference ID uniquely identifies the user as required by the
+%% spec, there is no constraint on the number of RefID's associated
+%% with a given user.
+
 start_link(UserName) ->
-    PriceList = [{donuts,50}, {macarons,175},{danish,100},{cupcakes,75}],
-    ReferenceId = spawn(?MODULE, init, [UserName, PriceList]),
+    Prices = [{donuts,50}, {macarons,175},{danish,100},{cupcakes,75}],
+    ReferenceId = spawn(?MODULE, init, [UserName, Prices]),
     {ok, ReferenceId}.
 
 donuts(ReferenceId, N) -> 
@@ -34,8 +42,18 @@ cupcakes(ReferenceId, N) ->
     send(ReferenceId, {order, cupcakes, N}).
 
 view_cart(ReferenceId) ->
-    send(ReferenceId, view).
+    sync_send(ReferenceId, view).
     
+billing_address(ReferenceId, Address) ->
+    sync_send(ReferenceId, {address, Address}).
+
+credit_card(ReferenceId, Number, Date) ->
+    sync_send(ReferenceId,{credit, Number, Date}).
+
+buy(ReferenceId) ->
+    sync_send(ReferenceId, buy).
+
+
 %% API Impl
 send(Pid, Message) ->
     Pid ! {request, self(), Message},
@@ -44,24 +62,39 @@ send(Pid, Message) ->
 	    Reply
     end.
 
+sync_send(Pid, Message) ->
+    Pid ! {sync_request, self(), Message},
+    receive
+	{reply, Reply} ->
+	    Reply
+    end.
 
 
 %% Implementation
-init(UserName, PriceList) ->
-    io:format("cart (~p) - initialising with price list ~p~n", [UserName, PriceList]),
-    InitialState = [ {Item, 0, 0}||{Item, _Price} <- PriceList],
-    loop(UserName, PriceList, InitialState).
+init(UserName, Prices) ->
+    io:format("cart (~p) - initialising with price list ~p~n", 
+	      [UserName, Prices]),
+    InitialState = [ {Item, 0, 0}||{Item, _Price} <- Prices],
+    loop(#user{name=UserName}, InitialState, Prices).
 
 
-loop(UserName, PriceList, State) ->
-    io:format("cart (~p) - looping with state ~p~n", [UserName, State]),
+
+loop(User, State, Prices) ->
+    io:format("cart (~p) - looping with state ~p~n", [User, State]),
     receive
-	{stop, Pid} ->
-	    io:format("cart (~p) - received stop~n", [UserName]),
-	    Pid ! ok;
 	{request, Pid, Message} ->
-	    Pid! {reply,ok},
-	    loop(UserName, PriceList, handle_request(Message, State, PriceList));
+	    reply(Pid,ok),
+	    {NewUser, NewState} = handle_request(Message, User,State,Prices),
+	    loop(NewUser, NewState, Prices);
+	{sync_request, Pid, Message} ->
+	    
+	    case sync_request(Message, User, State, Prices) of 
+		{NewUser,NewState,Response} ->
+		    reply(Pid,Response),
+		    loop(NewUser, NewState, Prices);
+		{stop, Response} ->
+		    reply(Pid,Response)
+	    end;
 	Msg -> io:format("cart - Unexpected message: ~p~n", [Msg])
     end.
 
@@ -69,14 +102,59 @@ loop(UserName, PriceList, State) ->
 
 
 
-handle_request({order, Item, N}, State, PriceList) ->
-    order(Item, N, State, PriceList);
-handle_request(view, State, _PriceList) ->
-    {_, _, SubTotals} = lists:unzip3(State),
-    {[ {Item, N} || {Item, N, _SubTotal} <- State],
-     lists:sum(SubTotals)}.
+handle_request({order,Item,N}, User,State,Prices) ->
+    {User,  order(Item,N,State,Prices)}.
+
+sync_request(view, User, State, _Prices) ->
+    {User,State,invoice(State)};
+sync_request({credit, Number,Date}, User, State, _Prices) ->
+    {NewUser, Response}=set_credit_card(User,Number,Date),
+    {NewUser, State, Response};
+sync_request({address,Address}, User, State, _Prices) ->
+    {NewUser,Response} = set_address(User,Address),
+    {NewUser,State,Response};
+sync_request(buy, User, State,_Prices) ->
+    {User, NewState, Response} = buy(User,State),
+    {User, NewState,Response}.
     
 
+
+
+
+
+
+reply(Pid, Message) ->
+    Pid ! {reply, Message}.
+
+set_address(User,Address) ->
+    {User#user{address=Address}, ok}. % TODO error cases
+
+set_credit_card(User,Number,Date) ->
+    {User#user{card={Number,Date}}, ok}. % TODO error cases
+
+
+%% Valid buy requests have defined values for a user's address and
+%% credit card info.  In that case the shopping basket is closed once
+%% order confirmation is sent,
+
+buy(#user{address=undefined} =U, State) ->
+    {U, State, {error, billing_info}}; 
+buy(#user{card=undefined} = U, State) ->
+    {U, State, {error, credit_info}};
+buy(User, State) ->
+    {stop, {ok,invoice(State)}}.
+
+
+
+%% Given a list of order tuples of the form {Item, Count, SubTotal},
+%% returns a tuple of {[Item,Count], Total} 
+invoice(State) ->    
+{_, _, SubTotals} = lists:unzip3(State),
+{[ {Item, N} || {Item, N, _SubTotal} <- State], lists:sum(SubTotals)}.
+
+
+
+    
 
 
 %% Init state to have zeros for all price list
@@ -91,9 +169,9 @@ handle_request(view, State, _PriceList) ->
     %% early if a non-existent item is ordered.
 
 %% order - > returns new order
-order(Item, N, Order, PriceList) ->
+order(Item, N, Order, Prices) ->
     io:format("Ordering ~p with current order of ~p~n", [Item, Order]),
-    {Item, Price} = lists:keyfind(Item, 1, PriceList),
+    {Item, Price} = lists:keyfind(Item, 1, Prices),
     io:format("looking up current order~p~n", [Order]),
     {Item, Quantity, SubTotal} = lists:keyfind(Item, 1, Order),
     
@@ -114,6 +192,15 @@ modify_item(Item, _N, _T) ->
 
 
 
+
+test() ->
+    {ok,TestPid} = start_link(my_user),
+    ok = macarons(TestPid, 3),
+    ok = macarons(TestPid, -2),
+    ok = macarons(TestPid, -3),
+    ok = donuts(TestPid,3),
+    view_cart(TestPid),
+    {error, billing_info} = buy(TestPid).
 
 			    
 		 
